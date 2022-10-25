51aaee2966f7f5c6d7c05e93bb57bb2f10907005
Sahith Kota <sahithkota@Sahiths-MacBook-Air.local>
2022-10-24 23:18:39 -0400

    a6 React.js sp22

--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -1,10 +1,27 @@
 import Nav from "../nav";
-function Tuiter() {
- return (
-   <div>
-     <Nav/>
-     <h1>Tuiter</h1>
-   </div>
- )
-}
-export default Tuiter
+import ExploreComponent from "./explore";
+import NavigationSidebar from "./navigation-sidebar";
+import WhoToFollowList from "./who-to-follow-list";
+import HomeComponent from "./home"
+import {Routes, Route} from "react-router";
+
+
+  function Tuiter() {
+
+    return(
+        <div>
+        <div className="row mt-2">
+        <div className="col-2 col-md-2 col-lg-1 col-xl-2">
+        <NavigationSidebar />
+        </div>
+        <div className="col-10 col-md-10 col-lg-11 col-xl-10">
+        <Routes>
+         <Route path="home"    element={<HomeComponent/>}/>
+         <Route path="explore" element={<ExploreComponent/>}/>
+       </Routes>
+       </div>
+        </div>
+       </div>    
+    );
+ }
+ export default Tuiter
